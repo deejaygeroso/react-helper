7b5f6f20f1777fa57612f6d19670d4da8b030afb
Deejay Geroso <deejaygeroso@gmail.com>
2023-02-24 22:26:28 +0800

    Update: Comment updated

--- packages/config/eslint-config/index.js
+++ packages/config/eslint-config/index.js
@@ -3,12 +3,7 @@ module.exports = {
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:react-hooks/recommended', // Enforces Rules of React Hooks
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    /* -----------------------------------------------------------------------------------------------------------------
-     * Enables eslint-plugin-prettier and displays prettier errors as ESLint errors.
-     * THe plugin:prettier/recommended config sets up both eslint-plugin-prettier and eslint-config-prettier in one go
-     * Make sure this is always the last configuration in the extends array.
-     * ----------------------------------------------------------------------------------------------------------------- */
-    'prettier',
+    'prettier', // Turns off all rules that are unnecessary or might conflict with Prettier.
   ],
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
