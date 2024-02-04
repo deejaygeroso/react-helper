24c23ff32170fc8a46016f1508745fbd1ef34032
Deejay Geroso <deejaygeroso@gmail.com>
2024-02-04 14:18:31 +0800

    Initialized tailwind

--- packages/ui/src/lib/Button/Button.types.ts
+++ packages/ui/src/lib/Button/Button.types.ts
@@ -1,4 +1,6 @@
 import { ButtonProps as MuiButtonProps } from '@mui/base';
 
-export type ButtonProps = MuiButtonProps
+export interface ButtonProps extends MuiButtonProps {
+  variant?: 'contained' | 'outlined';
+}
 
