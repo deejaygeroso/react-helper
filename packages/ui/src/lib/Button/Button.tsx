bc2cefb977d792b4ca73d42b171a8a390d0a54e1
Deejay Geroso <deejaygeroso@gmail.com>
2024-02-04 15:13:50 +0800

    Update colors and added source code

--- packages/ui/src/lib/Button/Button.tsx
+++ packages/ui/src/lib/Button/Button.tsx
@@ -4,11 +4,14 @@ import { ButtonProps } from './Button.types';
 
 function Button(props: Readonly<ButtonProps>): ReactElement {
   const { variant = 'contained', ...otherProps } = props;
+  const commonClasses =
+    'brfont-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:ring-4 focus:outline-none dark:focus:ring-blue-800';
+
   if (variant === 'outlined') {
     return (
       <button
         type='button'
-        className='text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800'
+        className={`${commonClasses} text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500`}
         {...otherProps}
       />
     );
@@ -17,7 +20,7 @@ function Button(props: Readonly<ButtonProps>): ReactElement {
   return (
     <button
       type='button'
-      className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+      className={`${commonClasses} text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700`}
       {...otherProps}
     />
   );
